2b84470ab8ec7e16b25b8fd971edfc7938cd0c82
dennishurst <dennis.hurst@saltworkssecurity.com>
2014-12-11 17:58:04 -0500

    FixTheCode checkin
    With this check-in you will have access to the fall final exam.  Good
    luck and let me know if you need assistance.

--- Dennis.Hurst/jQuery/script.js
+++ Dennis.Hurst/jQuery/script.js
@@ -1,13 +1,60 @@
+var mySlider = {
+	isSliderDown : false,
+	SliderColor : "red"
+};
 
-//alert('hello');
+
+
+/*
+ * Anonymous version
+ * 
+
 $(document).ready(function(){ 
 	//alert('ready');
-    $('.pull-me').click(function(){
-    	//alert('hello');
+    $('.pull-me').click(function () {
+   		if (mySlider.isSliderDown){
+    		mySlider.isSliderDown = false;
+    	}
+    	else {
+    		mySlider.isSliderDown = true;
+    	}
         $('.panel').slideToggle('slow');
-        
-    });
-    
-    
-    
+	});
 });
+*/
+
+var clickMe = function(){
+    
+    if (mySlider.isSliderDown){
+        alert('the slider is down');
+    }
+    else {
+        alert('the slider is up');
+    }
+}
+
+var myClickThing = function () {
+    alert('hello from myClickThing');
+    if (mySlider.isSliderDown){
+        mySlider.isSliderDown = false;
+    }
+    else {
+        mySlider.isSliderDown = true;
+    }
+    
+    $('.panel').slideToggle('slow');
+}
+
+
+
+
+
+$(document).ready(function(){
+	$('.pull-me').click(myClickThing());
+});
+
+
+
+
+
+
