df634219b54118309c916883aa739c448e37665e
X-terminator <joshuameder@gmail.com>
2014-12-11 18:45:21 -0500

    Objects I
    Objects I

---
+++ Josh.Meder/objects.js
@@ -0,0 +1,12 @@
+function Circle (radius) {
+    this.radius = radius;
+    this.area = function () {
+        return Math.PI * this.radius * this.radius;
+        
+    };
+    // define a perimeter method here
+    this.perimeter = function() {
+        return 2 * Math.PI * this.radius;
+    };
+}
+
