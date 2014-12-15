09be52881ff3e593a4d50d1dd23fcd07c60debf7
X-terminator <joshuameder@gmail.com>
2014-12-14 21:36:20 -0500

    ObjectsII
    ObjectsII

---
+++ Josh.Meder/ObjectsII.js
@@ -0,0 +1,18 @@
+function StudentReport() {
+    var grade1 = 4;
+    var grade2 = 2;
+    var grade3 = 1;
+    this.getGPA = function() {
+        return (grade1 + grade2 + grade3) / 3;
+    };
+}
+
+var myStudentReport = new StudentReport();
+
+for(var x in myStudentReport) {
+    if(typeof myStudentReport[x] !== "function") {
+        console.log("Muahaha! " + myStudentReport[x]);
+    }
+}
+
+console.log("Your overall GPA is " + myStudentReport.getGPA());
